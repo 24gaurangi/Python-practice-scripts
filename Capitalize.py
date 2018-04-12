0b782a0f90161ab71c088b5b4a46e99a443a7df4
gwcoder <gwanjari19@gmail.com>
2018-04-12 16:02:31 -0700

    Added Capitalize.py

---
+++ Capitalize.py
@@ -0,0 +1,21 @@
+'''
+This program takes the input string and capitalizes first letter of every word in the string
+'''
+
+def LetterCapitalize(str): 
+    new_str=""
+    cc=False
+    for i in range(len(str)):
+        if i == 0:
+            new_str=new_str + str[i].upper()
+        elif str[i] == " ":
+            cc=True
+            new_str=new_str + str[i]
+        elif cc:
+            new_str=new_str + str[i].upper()
+            cc=False
+        else:
+            new_str=new_str + str[i]
+    return new_str
+      
+print LetterCapitalize(input("Please enter a string to capitalize\n"))
